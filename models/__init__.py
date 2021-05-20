c0539f6ba1419da32efa4a7ee0b10b78f5e5d414
Peter Macaluso <moxious.manoeuvre@gmail.com>
2021-05-20 09:55:01 -0400

    explain models folder

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,7 @@ from .Post import Post
 from .Comment import Comment
 from .Topic import Topic
 from .Tagging import Tagging
+
+# when you import models, this file is all that python sees from the folder
+# so this file is used to package up the whole folder
+# ie anything that we want to be accessible via models.whatever needs to be imported into this file
